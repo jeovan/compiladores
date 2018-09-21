e1a1039823bbe71910b6c02531e8fa521ea44d4c
jeovan <jeovan.farias@sefin.ro.gov.br>
2018-09-21 13:05:17 -0400

    alteracoes

--- ruby/main.rb
+++ ruby/main.rb
@@ -2,8 +2,7 @@ require 'yaml'
 require_relative './analisador_lexico/Analyzer.rb'
 
 scan = Analyzer.new(YAML.load_file(File.join(__dir__, './analisador_lexico/rules.yml')))
-# scan.ignore_tokens = 'COMMENT'
+scan.ignore_tokens = 'COMMENT'
 # scan.errors = true
 p = scan.tokenizer_file('./analisador_lexico/teste.nsp')
-
 p.each{|d| puts "[#{d[:line] }]#{d[:type] } => #{d[:value] }"}
